90884d0245df1c1030aed90108438056382b7458
Blake Epstein <blake@brightforestmedia.com>
2020-06-01 18:22:54 -0500

    updates

--- cookieJar.js
+++ cookieJar.js
@@ -2,11 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
     function refSource() {
         if (document.referrer === "") {
             return "direct";
-        } else if (document.referrer.search("https?://(.*)google.([^/?]*)") === 0) {
+        } else if (
+            document.referrer.search("https?://(.*)google.([^/?]*)") === 0
+        ) {
             return "google";
-        } else if (document.referrer.search("https?://(.*)bing.([^/?]*)") === 0) {
+        } else if (
+            document.referrer.search("https?://(.*)bing.([^/?]*)") === 0
+        ) {
             return "bing";
-        } else if (document.referrer.search("https?://(.*)yahoo.([^/?]*)") === 0) {
+        } else if (
+            document.referrer.search("https?://(.*)yahoo.([^/?]*)") === 0
+        ) {
             return "yahoo";
         } else if (
             document.referrer.search("https?://(.*)facebook.([^/?]*)") === 0
@@ -28,11 +34,17 @@ document.addEventListener("DOMContentLoaded", function() {
             document.referrer.search("https?://(.*)instagram.([^/?]*)") === 0
         ) {
             return "instagram";
-        } else if (document.referrer.search("https?://(.*)adroll.([^/?]*)") === 0) {
+        } else if (
+            document.referrer.search("https?://(.*)adroll.([^/?]*)") === 0
+        ) {
             return "adroll";
-        } else if (document.referrer.search("https?://(.*)criteo.([^/?]*)") === 0) {
+        } else if (
+            document.referrer.search("https?://(.*)criteo.([^/?]*)") === 0
+        ) {
             return "criteo";
-        } else if (document.referrer.search("https?://(.*)ar1d.([^/?]*)") === 0) {
+        } else if (
+            document.referrer.search("https?://(.*)ar1d.([^/?]*)") === 0
+        ) {
             return "adroll";
         } else {
             return "other";
@@ -63,15 +75,15 @@ document.addEventListener("DOMContentLoaded", function() {
     function checkCookie() {
         var user = getCookie("_u");
         if (user == "") {
-            var source = urlParams.get("utm_source") ?
-                urlParams.get("utm_source") :
-                refSource();
-            var medium = urlParams.get("utm_medium") ?
-                urlParams.get("utm_medium") :
-                "organic_or_direct";
-            var campaign = urlParams.get("utm_campaign") ?
-                urlParams.get("utm_campaign") :
-                "organic_or_direct";
+            var source = urlParams.get("utm_source")
+                ? urlParams.get("utm_source")
+                : refSource();
+            var medium = urlParams.get("utm_medium")
+                ? urlParams.get("utm_medium")
+                : "organic_or_direct";
+            var campaign = urlParams.get("utm_campaign")
+                ? urlParams.get("utm_campaign")
+                : "organic_or_direct";
             setCookie("_u", true, 365);
             setCookie("_r", refSource(), 365);
             setCookie("_s", source, 365);
@@ -81,8 +93,8 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     checkCookie();
     $("a").click(function(e) {
-        e.preventDefault();
         if ($(this)[0].href.includes("formstack")) {
+            e.preventDefault();
             var href = $(this)[0].href;
             var source = getCookie("_s");
             var medium = getCookie("_m");
@@ -90,8 +102,6 @@ document.addEventListener("DOMContentLoaded", function() {
             var referrer = getCookie("_r");
             queryString = `?utm_source=${source}&utm_medium=${medium}&utm_campaign=${campaign}&utm_content=${referrer}`;
             window.location.href = `${href}${queryString}`;
-        } else {
-            window.location.href = e.href
         }
     });
 });
